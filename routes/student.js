b7ac6efb9bbaeb4d9ee11d101a241b4831497e34
Ian Marquez <regilmarquez123@gmail.com>
2022-02-03 14:42:11 +0800

    Update dependencies

--- routes/student.js
+++ routes/student.js
@@ -1,17 +1,15 @@
 module.exports = function(){
-	var express = require("express");
-	var app = new express.Router();
-	var http = require('http');
-	var cookieParser = require('cookie-parser');
-	var bodyParser = require('body-parser');
+	const express = require("express");
+	const app = new express.Router();
+	const bodyParser = require('body-parser');
 
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 
-	var connection  = require('express-myconnection'); 
-	var mysql = require('mysql');
+	const connection  = require('express-myconnection'); 
+	const mysql = require('mysql');
 
-	var pool = mysql.createPool({
+	const pool = mysql.createPool({
 		connectionLimit : 100, //important
 		host     : 'localhost',
 		user     : 'root',
@@ -21,7 +19,7 @@ module.exports = function(){
 
 	app.get('/getStudents',function(req,res){
 		pool.getConnection(function(err, connection){
-			var selectAllStudents = 'SELECT * FROM studentstbl';
+			const selectAllStudents = 'SELECT * FROM studentstbl';
 			if(err){
 				res.json({"code" : 100, "status" : "Error in connection database"});
 				return;
@@ -42,12 +40,12 @@ module.exports = function(){
 				res.json({"code" : 100, "status" : "Error in connection database"});
 				return;
 			}else{
-				var firstname = req.body.firstname;
-				var lastname = req.body.lastname;
-				var email = req.body.email;
-				var contactno = req.body.contactno;
+				const firstname = req.body.firstname;
+				const lastname = req.body.lastname;
+				const email = req.body.email;
+				const contactno = req.body.contactno;
 
-				var insertStudent = 'INSERT INTO studentstbl(firstName,lastName,contactNo,emailAddress) VALUE("'+firstname+'","'+lastname+'","'+contactno+'","'+email+'")';
+				const insertStudent = 'INSERT INTO studentstbl(firstName,lastName,contactNo,emailAddress) VALUE("'+firstname+'","'+lastname+'","'+contactno+'","'+email+'")';
 				connection.query(insertStudent, function(err, rows){
 					if(!err){
 						res.json({"code" : 300, "status" : "Student Successfully Inserted"});
@@ -65,13 +63,13 @@ module.exports = function(){
 				res.json({"code" : 100, "status" : "Error in connection database"});
 				return;
 			}else{
-				var id = req.body.id;
-				var firstname = req.body.firstname;
-				var lastname = req.body.lastname;
-				var email = req.body.email;
-				var contactno = req.body.contactno;
+				const id = req.body.id;
+				const firstname = req.body.firstname;
+				const lastname = req.body.lastname;
+				const email = req.body.email;
+				const contactno = req.body.contactno;
 
-				var updateStudent = 'UPDATE studentstbl SET firstName="' + firstname +'", lastName="' + lastname+ '", contactNo ="' + contactno + '", emailaddress ="' + email + '" WHERE studID=' + id;
+				const updateStudent = 'UPDATE studentstbl SET firstName="' + firstname +'", lastName="' + lastname+ '", contactNo ="' + contactno + '", emailaddress ="' + email + '" WHERE studID=' + id;
 				connection.query(updateStudent, function(err, rows){
 					if(!err){
 						res.json({"code" : 300, "status" : "Student Successfully Updated"});
@@ -82,17 +80,17 @@ module.exports = function(){
 			}
 		});
 	});
-	app.post('/deleteStudent', function(req, res){
+	app.post('/deconsteStudent', function(req, res){
 		pool.getConnection(function(err, connection){
 			if(err){
 				res.json({"code" : 100, "status" : "Error in connection database"});
 				return;
 			}else{
-				var id = req.body.id;
-				var deleteStudent = 'DELETE FROM studentstbl WHERE studID = "' + id + '"';
-				connection.query(deleteStudent, function(err, rows){
+				const id = req.body.id;
+				const deconsteStudent = 'DEconstE FROM studentstbl WHERE studID = "' + id + '"';
+				connection.query(deconsteStudent, function(err, rows){
 					if(!err){
-						res.json({"code" : 300, "status" : "Student Successfully Deleted"});
+						res.json({"code" : 300, "status" : "Student Successfully Deconsted"});
 					}else{
 						res.json({"code" : 100, "status" : "Error in executing query"});
 					}
